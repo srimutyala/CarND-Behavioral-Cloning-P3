387a322b878aba7642312ab2d559647ad8eedb9a
srimutyala <srimutyala@users.noreply.github.com>
2017-08-01 09:17:22 -0500

    Add files via upload

--- model.py
+++ model.py
@@ -10,8 +10,10 @@ from keras.layers import Convolution2D, MaxPooling2D, Cropping2D
 from keras.applications.inception_v3 import InceptionV3
 
 lines = []
+# A steering bias (positive or negative) for the left and right cameras
 steering_correction = 0.4
-#
+
+#Read the driving (training) data collected from simulator
 with open('driving_log.csv', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
@@ -19,8 +21,10 @@ with open('driving_log.csv', encoding='utf-8') as csvfile:
 
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
+#Split the data test to have a training set and a validation set
 train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
+#Define a generator to better manage the large set of training data (images)
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1:
@@ -30,6 +34,7 @@ def generator(samples, batch_size=32):
 
             images = []
             measurements = []
+            #Using the left and right camera images to augment the training
             for line in batch_samples:
                 image_center = cv2.imread(line[0])
                 image_left = cv2.imread(line[1])  # line[1][1:]
@@ -48,13 +53,14 @@ def generator(samples, batch_size=32):
             augmented_images, augmented_measurements = [], []
             # augmented_images = images
             # augmented_measurements = measurements
+
+            #Flipping the training data to balance the left-turn heavy track
             for image, measurement in zip(images, measurements):
                 augmented_images.append(image)
                 augmented_measurements.append(measurement)
                 augmented_images.append(cv2.flip(image, 1))
                 augmented_measurements.append(measurement * -1.0)
 
-            # trim image to only see section with road
             X_train = np.array(augmented_images)
             y_train = np.array(augmented_measurements)
             #yield sklearn.utils.shuffle(X_train, y_train)
@@ -68,10 +74,12 @@ validation_generator = generator(validation_samples, batch_size=32)
 model = Sequential()
 
 #ch, row, col = 3, 80, 320  # Trimmed image format
-## Preprocess incoming data, centered around zero with small standard deviation
 #model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(ch, row, col), output_shape=(ch, row, col)))
 
+#Preprocess incoming data, centered around zero with small standard deviation
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
+
+#Trim image to only see section with road
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 
 #model.add(Convolution2D(32, 7, 7, subsample=(2,2), border_mode='same', activation="relu"))
